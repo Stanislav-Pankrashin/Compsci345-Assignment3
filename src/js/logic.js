8c83b3e7c6e9f60a4563ac9bf1c9cd098d13a1de
Stanislav Pankrashin <stasera2@gmail.com>
2017-05-22 18:59:35 +1200

    Started adding javascript functionality

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,12 +1,32 @@
+/* sets the domain of every page to prevent crossdomain issues*/
+window.addEventListener('load', setDomain);
+
+function setDomain(){
+    document.domain = '';
+}
+
+
 /* function which changes the iframe using the sidebar buttons*/
 function sidebarButtonChanger(id){
-    var path = 'html/' + id + "_page.html";
-    var header = id + " page";
+    var path = "html/" + id + "_page.html";
+    var header = id[0].toUpperCase() + id.slice(1) + " Page";
+
+    var newIframe = "<iframe id = 'pageLinker' src=" + path + " width='100%' height='100%'></iframe>";
+
+    $("#pageName").html(header);
+
+    $("#pageLinker").attr("src", path);
+}
+
+/*Sets the page to the drama summary*/
+function toDramaSummary(){
+    var path = 'play_summary.html';
+    var header = "Play Summary Page";
 
     var newIframe = "<iframe id = 'pageLinker' src=" + path +" width='100%' height='100%'></iframe>";
 
     $("#pageName").html(header);
 
-    $("#pageLinker").attr("src", path);
+    window.parent.$("#pageLinker").attr("src", path);
 }
 
